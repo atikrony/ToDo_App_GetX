f69d09cd08c683938c70fe108d7f6ded66f67781
atik.rony <atik.rony2013@gmail.com>
2024-05-22 01:12:56 +0600

    dont get much time to do code, but here's another small thing i learned today

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -12,62 +12,64 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: AppColor.backgroundColor,
-        child: Padding(
-          padding: const EdgeInsets.all(15),
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text(
-                  "My Nudes",
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  children: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.menu),
-                    ),
-                    const Spacer(),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.search),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.more_vert),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Expanded(
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        ListView.builder(
-                          shrinkWrap: true,
-                          primary: false,
-                          itemCount: 20,
-                          itemBuilder: (context, index) {
-                            return const CustomSingleNotes(
-                                title: "Test",
-                                description: "this is test",
-                                date: "12.12.12");
-                          },
-                        ),
-                      ],
+      body: SafeArea(
+        child: Container(
+          color: AppColor.backgroundColor,
+          child: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    "My Nudes",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.menu),
+                      ),
+                      const Spacer(),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.search),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.more_vert),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          ListView.builder(
+                            shrinkWrap: true,
+                            primary: false,
+                            itemCount: 20,
+                            itemBuilder: (context, index) {
+                              return const CustomSingleNotes(
+                                  title: "Test",
+                                  description: "this is test",
+                                  date: "12.12.12");
+                            },
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 65,
-                ),
-              ],
+                  const SizedBox(
+                    height: 65,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -76,7 +78,7 @@ class HomeScreen extends StatelessWidget {
         shape: const CircleBorder(),
         backgroundColor: AppColor.primaryColor,
         onPressed: () => Get.toNamed(RouteNames.noteScreen),
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
     );
   }
--- lib/view/note/note_screen.dart
+++ lib/view/note/note_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:getx/utils/colors.dart';
 
 class NoteScreen extends StatelessWidget {
   const NoteScreen({super.key});
@@ -7,7 +7,61 @@ class NoteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        scrolledUnderElevation: 0,
+        backgroundColor: AppColor.backgroundColor,
+        bottom: const PreferredSize(
+          preferredSize: Size.fromHeight(1),
+          child: Divider(),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.link),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.share),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          children: [
+            TextFormField(
+              style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              minLines: 1,
+              maxLines: 2,
+              decoration: const InputDecoration(
+                  hintText: "Title",
+                  border: InputBorder.none,
+                  hintStyle:
+                      TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+            ),
+            Expanded(
+              child: TextFormField(
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                minLines: 10,
+                maxLines: 50,
+                decoration: const InputDecoration(
+                    hintText: "Description",
+                    border: InputBorder.none,
+                    hintStyle:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
