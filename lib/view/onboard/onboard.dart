366d60e8d961347ef2dad36dd041f6515e8a5362
atik.rony <atik.rony2013@gmail.com>
2024-06-01 23:54:02 +0600

    didn't know i have to do more in this shit !

--- lib/view/onboard/onboard.dart
+++ lib/view/onboard/onboard.dart
@@ -5,6 +5,12 @@ class OnBoardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          ("onboard"),
+        ),
+      ),
+    );
   }
 }
