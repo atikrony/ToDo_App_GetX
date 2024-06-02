9c3524fb9306e35136bb5697cab5b59588f51719
atik.rony <atik.rony2013@gmail.com>
2024-06-03 04:09:27 +0600

    wrong code !

--- lib/view/onboard/onboard.dart
+++ lib/view/onboard/onboard.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 
 class OnBoardScreen extends StatelessWidget {
   const OnBoardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text(
-          ("onboard"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.25,
+              width: MediaQuery.of(context).size.width * 0.50,
+              decoration: BoxDecoration(
+                border: Border.all(width: 1, color: Colors.black),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Lottie.asset("assets/lottie/ham.json"),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+          ],
         ),
       ),
     );
